541e717f08990234ff94fab3e99ec44987716b46
zql1520 <2232770251@qq.com>
2019-06-23 21:37:34 +0800

    Initial commit

---
+++ test1.py
@@ -0,0 +1,36 @@
+# test1 2019/06/23  搭建编程环境及Python的基本了解
+
+print("Hello World")
+
+"""
+初始化一个新的Git仓库
+git config --global user.name 'zql****' (设置用户名)
+git config --global user.email '**********@qq.com' (设置用户名邮箱)
+mkdir 创建文件夹
+cd test1
+git init 显示隐藏文件
+提交到本地git
+git add test1.py
+git status
+git commit -m 'add test1.py'
+添加到远程仓库
+git push
+
+Python优缺点：
+易学，语法简单，容易上手
+开源
+可移植，可以在不同的平台上运行
+丰富的库
+python的应用领域：
+web应用开发
+网络爬虫
+桌面软件
+操作系统管理
+自动化测试
+
+注释的作用：
+对代码的解释和说明，提高代码的可读性。
+单行注释#
+多行注释(""" """)
+快捷注释Ctrl+/
+"""
