0f0aa0d768463237839c9d590cf80fcf7eec57dd
zql1520 <2232770251@qq.com>
2019-07-03 23:39:44 +0800

    if语句和while循环

--- PythonLearing/Test/test3.py
+++ PythonLearing/Test/test3.py
@@ -21,24 +21,29 @@ bug会导致程序执行结果不是预期
 """
 
 
-# # # 猜拳游戏
-# # # 导入随机数
-# # import random
-# #
-# # 获取用户的拳头
-# user_quan = int(input('请输入：石头（0）剪刀（1）布（2）'))
-# # 获取系统的拳头
-# computer_quan = random.randint(0,2)
-# # 判断胜负
-# # 用户赢的情况
-# if (user_quan == 0 and computer_quan == 1) or \
-#     (user_quan == 1 and computer_quan == 2) or \
-#     (user_quan == 2 and computer_quan == 0):
-#     print('您赢了！')
-# elif user_quan == computer_quan:
-#     print('平局！')
-# else:
-#     print('您输了！')
+# 猜拳游戏
+# 导入随机数
+import random
+
+while True:
+    # 获取用户的拳头
+    user_quan = int(input('请输入：石头（0）剪刀（1）布（2）'))
+    if user_quan == 3:
+        print('退出游戏！')
+        break
+    # 获取系统的拳头
+    computer_quan = random.randint(0,2)
+
+    # 判断胜负
+    # 用户赢的情况
+    if (user_quan == 0 and computer_quan == 1) or \
+        (user_quan == 1 and computer_quan == 2) or \
+        (user_quan == 2 and computer_quan == 0):
+        print('您赢了！')
+    elif user_quan == computer_quan:
+        print('平局！')
+    else:
+        print('您输了！')
 
 
 # 重复的代码越多，后期维护的工作量就越大，成本越高
@@ -113,25 +118,64 @@ bug会导致程序执行结果不是预期
 # break 执行，后面的代码不再执行，并且马上终止循环
 
 
-# 水仙花数  （未执行完）
-# import
-# 表示水仙花数每个位上的数字
-for n in range(100,999,1):
-# 给出n的三个数字
-    a = n // 100
-    b = n //10 % 10
-    c = n % 10
-# 写出求和公式
-my_sum = int(a ** 3 + b ** 3 + c ** 3)
-while range(100,999,):
-    if n != my_sum:
-        continue
-    else:
-       print('水仙花数是：',n)
+# # 水仙花数  （未执行完）
+# # import
+# # 表示水仙花数每个位上的数字
+# for n in range(100,999):
+# # 给出n的三个数字
+#     a = n // 100
+#     b = n //10 % 10
+#     c = n % 10
+# # 写出求和公式
+# my_sum = int(a ** 3 + b ** 3 + c ** 3)
+# while range(100,999,):
+#     if n != my_sum:
+#         continue
+#
+# print('水仙花数是：d%',n)
+
+#  简易版的员工管理系统
+# 1 接受用户输入
+# 2.1 展示所有员工信息
+# 2.2 新增一个员工信息
+# 2.3 修改一个员工信息
+# 2.4 删除一个员工信息
+# 2.5 退出员工管理系统
 
 
-
-
-
+# print('欢迎使用【员工管理系统v1.0】')
+#
+# while True:
+#     # 展示系统菜单
+#     print('*' * 10 + '操作菜单' + '*' * 10)
+#     print('1. 展示所有员工信息')
+#     print('2. 新增一个员工信息')
+#     print('3. 修改一个员工信息')
+#     print('4. 删除一个员工信息')
+#     print('5. 退出员工管理系统')
+#     print('*' * 26)
+#     # 保存用户操作
+#     user_operration = int(input('请输入您的操作：'))
+#     if user_operration == 1:
+#         print('姓名\t年龄\t性别\t')
+#         print('小米\t34\t男\t')
+#     elif user_operration == 2:
+#         name = input('请输入姓名：')
+#         age = input('请输入年龄：')
+#         rex = input('请输入性别：')
+#         print('新员工 %s 添加到系统成功！' % name)
+#     elif user_operration == 3:
+#         name = input('请输入你要修改的员工：')
+#         print('员工 %s 的信息修改成功！' % name)
+#     elif user_operration == 4:
+#         name = input('请输入你要删除的员工姓名：')
+#         print('员工 %s 已被成功删除！' % name)
+#
+#     elif user_operration == 5:
+#         print('退出员工管理系统成功！')
+#         print('欢迎再次使用！')
+#         break
+#     else:
+#         print('输出有误！')
 
 
